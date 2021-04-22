237ad4f9d588a3f6e23d96ace036ca5123461343
safwan <safwan@csc.net.my>
2021-04-22 13:19:19 +0800

    add dataTable on Rekod Transaksi & pendaftaran (info emandate)

--- resources/views/pages/emandateInfo/rekod-pendaftaran.blade.php
+++ resources/views/pages/emandateInfo/rekod-pendaftaran.blade.php
@@ -1,24 +1,28 @@
 <x-general.grid mobile="1" gap="5" sm="1" md="1" lg="1" xl="1" class="col-span-6 px-2">
-    <x-table.table>
-        <x-slot name="thead">
-            <x-table.table-header class="text-left" value="Rekod" sort="" />
-            <x-table.table-header class="text-left" value="Tarikh Berjaya Daftar" sort="" />
-            <x-table.table-header class="text-left" value="Status" sort="" />
-        </x-slot>
-        <x-slot name="tbody">
-            @foreach ($INFOS as $item)
-                <tr>
-                    <x-table.table-body colspan="" class="text-sm font-medium text-gray-700 ">
-                        {{ $item->recnum }}
-                    </x-table.table-body>
-                    <x-table.table-body colspan="" class="text-sm font-medium text-gray-700 ">
-                        {{ isset($item->effdate) ? date('d-m-Y',strtotime($item->effdate)):'' }}
-                    </x-table.table-body>
-                    <x-table.table-body colspan="" class="text-sm font-medium text-gray-700 ">
-                        {{ $item->approved_desc }}
-                    </x-table.table-body>
-                </tr>
-            @endforeach
-        </x-slot>
-    </x-table.table>
+    <div class="flex flex-col mx-4">
+        <div class="-my-2 overflow-x-auto">
+            <div class="py-2 align-middle inline-block min-w-full">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg bg-white">
+                    <table class="data-table min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-800">
+                                <tr>
+                                    <th data-priority="1" class="uppercase text-xs text-white text-left">Rekod</th>
+                                    <th data-priority="2" class="uppercase text-xs text-white text-left">Tarikh Berjaya Daftar</th>
+                                    <th data-priority="3" class="uppercase text-xs text-white text-left">Status</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach ($INFOS as $item)
+                                    <tr>
+                                        <td class="text-sm text-left">{{ $item->recnum }}</td>
+                                        <td class="text-sm text-left">{{ isset($item->effdate) ? date('d-m-Y',strtotime($item->effdate)):'' }}</td>
+                                        <td class="text-sm text-left">{{ $item->approved_desc }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                    </table>  
+                </div>
+            </div>
+        </div>
+    </div>
 </x-general.grid>
